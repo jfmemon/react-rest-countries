f7fa2d7f027a6fcc1db0710e2d191b86f2c1f65d
jfmemon <jannatul15-2497@diu.edu.bd>
2023-03-23 00:56:58 +0600

    certain mistake solve

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Country.css';
 const Country = (props) => {
     // console.log(props.country);
-    const {area, flags, name, capital, ccn3} = props.country;  // destructuring
+    const {area, flags, name, capital} = props.country;  // destructuring
     return (
         <div className='country'>
             <img style={{width:'300px', padding:'5px'}} src={flags.png} alt=""/>
